743eace106168cb19f450af39c3b88e65267f2e5
kennqiang <kennqiang@163.com>
2017-09-09 10:51:53 +0800

    file write

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,12 +2,18 @@
 //
 
 #include "stdafx.h"
+#include <fstream>
 #include <iostream>
 using namespace std;
 
 
 int main()
 {
+	ofstream examplefile("examplefile.txt");
+	if (examplefile.is_open) {
+		examplefile << "line1\n";
+		examplefile << "line2\n";
+	}
 	cout << "hello world!";
     return 0;
 }
