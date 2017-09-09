3d3392a090c1c6293925bac3e93b18d0200b1748
kennqiang <kennqiang@163.com>
2017-09-09 17:55:24 +0800

    读文件出现最后一个字符输出了两遍

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -4,17 +4,24 @@
 #include "stdafx.h"
 #include <fstream>
 #include <iostream>
+#include <string>
 using namespace std;
 
 
 int main()
 {
-	ofstream examplefile("examplefile.txt");
-	if (examplefile.is_open()) {
-		examplefile << "line1\n";
-		examplefile << "line2\n";
+	ifstream in;
+	in.open("examplefile.txt");
+	if (in) {
+		while (!in.eof()) {
+			char c;
+			in.read(&c,1);
+			cout << c;
+		}
 	}
+	in.close();
 	cout << "hello world!";
+	getchar();
     return 0;
 }
 
